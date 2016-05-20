744e08f03f2667fef9ca299427a678ee12a45874
leblancfg <leblancfg@gmail.com>
2016-05-20 07:10:10 -0400

    changed use of _sm .jpeg images to the high-def .tif files. Hoping the numeric values won't be affected by pixel ratio change. If that happens, need to multiply those values by the image size ratio.

--- panorama.py
+++ panorama.py
@@ -1,10 +1,10 @@
 import numpy as np
-import imutils
+#import imutils
 import cv2
 
 class Stitcher:
-    def __init__(self):
-        self.isv3 = imutils.is_cv3()
+    #def __init__(self):
+        #self.isv3 = imutils.is_cv3()
         # determine if we are using OpenCV 3.x
     def stitch(self,images, ratio=0.75, reprojThresh=4.0):
     # unpack the images, then detect keypoints and extract
@@ -16,16 +16,17 @@ class Stitcher:
         M = self.matchKeypoints(kpsA,kpsB, featuresA,featuresB,ratio,reprojThresh)
         #if the match is None, then there aren't enough matched keypoints to create a panorama
         (matches, H, status) = M
+        #(hA, wA) = imageA.shape[:2]
+        #(hB, wB) = imageB.shape[:2]
+
         # Transparency
-        (hA, wA) = imageA.shape[:2]
-        (hB, wB) = imageB.shape[:2]
-        imageATran = np.zeros((hA,wA,3),np.uint8)
-        imageBTran = np.zeros((hB, wB, 3), np.uint8)
-        cv2.addWeighted(imageATran,0.6,imageA,0.6,-1,imageATran)
+        #imageATran = np.zeros((hA,wA,3),np.uint8)
+        #imageBTran = np.zeros((hB, wB, 3), np.uint8)
+        #cv2.addWeighted(imageATran,0.6,imageA,0.6,-1,imageATran)
         #cv2.addWeighted(imageBTran, 0.6, imageB, 0.6, -1, imageBTran)
-        P = [0,]
+
         #/Transparency
-        result = cv2.warpPerspective(imageATran, H,(imageA.shape[1]+imageB.shape[1],imageA.shape[0]+imageB.shape[0]))
+        result = cv2.warpPerspective(imageA, H,(imageA.shape[1]+imageB.shape[1],imageA.shape[0]+imageB.shape[0]))
         #result[0:imageB.shape[0],0:imageB.shape[1]] = imageBTran
         #result = cv2.warpPerspective(imageATran, H,(imageA.shape[1]+imageB.shape[1],imageA.shape[0]))
         #result[0:imageB.shape[0],0:imageB.shape[1]] = imageBTran
@@ -39,7 +40,7 @@ class Stitcher:
         #convert the image to grayscale
         gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
         descriptor = cv2.xfeatures2d.SIFT_create()
-        (kps,features) = descriptor.detectAndCompute(image,None)
+        (kps,features) = descriptor.detectAndCompute(gray,None)
         #convert the keypoints from KeyPoint objects to NumPy arrays
         kps = np.float32([kp.pt for kp in kps])
         #return a tuple of keypoints and features
@@ -47,11 +48,11 @@ class Stitcher:
         (kps, features) = detector.detectAndCompute(image, None)'''
         return (kps, features)
 
-
     def matchKeypoints(self, kpsA,kpsB,featuresA, featuresB, ratio, reprojThresh):
         matcher = cv2.DescriptorMatcher_create("BruteForce")
         rawMatches = matcher.knnMatch(featuresA,featuresB,2)
         matches=[]
+
         # loop over the raw matches
         for m in rawMatches:
             #ensure the distance is within a certain ration of each other (i.e. Lowe's ratio test)
@@ -59,26 +60,32 @@ class Stitcher:
                 matches.append((m[0].trainIdx,m[0].queryIdx))
         if(len(matches)<5):
             return None
+
         ptsA = np.float32([kpsA[i] for (_,i) in matches])
         ptsB = np.float32([kpsB[i] for (i,_) in matches])
         (H, status) = cv2.findHomography(ptsA,ptsB,cv2.RANSAC,reprojThresh)
+
         #return the matches along with the homography matrix and status of each matched points
         return (matches,H,status)
+
     def drawMatches(self,imageA,imageB,kpsA,kpsB,matches,status):
-        #initialize the outpu visualization image
+        #initialize the output visualization image
         (hA,wA) = imageA.shape[:2]
         (hB,wB) = imageA.shape[:2]
         vis = np.zeros((max(hA,hB), wA + wB, 3), dtype="uint8")
         vis[0:hA, 0:wA] = imageA
         vis[0:hB, wA:] = imageB
+
         #loop over the matches
         for ((trainIdx, queryIdx), s) in zip(matches,status):
             #only process the match if the keypoint was successfully matched
-            if s ==1:
+            if s == 1:
                 #draw the match
                 ptA = (int(kpsA[queryIdx][0]),int(kpsA[queryIdx][1]))
                 ptB = (int(kpsB[trainIdx][0])+wA,int(kpsB[trainIdx][1]))
                 cv2.line(vis,ptA,ptB,(0,255,0),1)
+            gc.collect()
+
         #return the visualisation
         return vis
 
--- stitch.py
+++ stitch.py
@@ -1,18 +1,30 @@
+import time
+start = time.time()
+
 from panorama import Stitcher
-import argparse
+#import argparse
 import imutils
 import cv2
 
-#construct the argument parse and parse the arguments
-imageA = cv2.imread('/mnt/hgfs/Kaggle Drapper/train/set12_1.tif')
-imageB = cv2.imread('/mnt/hgfs/Kaggle Drapper/train/set12_3.tif')
-imageA = imutils.resize(imageA,width=500)
-imageB = imutils.resize(imageB,width=500)
-stitcher = Stitcher()
-(result) = stitcher.stitch([imageA,imageB])
-print("Time to show")
+# construct the argument parse and parse the arguments
+imageA = cv2.imread('../input/train/set12_1.tif')
+imageB = cv2.imread('../input/train/set12_3.tif')
+reading = time.time()
+print('Read Images, time {0}'.format(reading-start))
+
+#cv2.imshow("image a", imageA)
+#cv2.waitKey(0)
+#imageA = imutils.resize(imageA,width=500)
+#imageB = imutils.resize(imageB,width=500)
+
+# Init Stitcher class, feed images to function
+stitcher_ = Stitcher()
+result = stitcher_.stitch([imageA, imageB])
+end = time.time()
+print("Finished stitching in {0}".format(end-reading))
+
 # show the images
+print("Time to show")
 #cv2.imshow("image a", imageA)
 #cv2.imshow("image b", imageB)
-cv2.imshow("Result",result)
-cv2.waitKey(0)
+cv2.imwrite("test.png",imutils.resize(result,width=500))
