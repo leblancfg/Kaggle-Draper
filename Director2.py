abb80fc956617dbe81088873418be8e7192c48fc
PsYc0p4tH <renie37@hotmail.com>
2016-06-22 05:26:31 -0700

    Pushing newly created stuffs.

--- Director2.py
+++ Director2.py
@@ -2,9 +2,13 @@ from os import listdir
 import numpy as np
 import imutils
 from os.path import isfile, join
+import os
 import cv2
+import time
+import math
+
 from chipy import  im_stitcher
-path = "/mnt/hgfs/Kaggle Drapper/train/"
+path = "train/"
 numbersoffileperset = 5
 onlyfiles = [f for f in listdir(path) if isfile(join(path,f))]
 composite_list = [onlyfiles[x:x+numbersoffileperset] for x in range(0,len(onlyfiles),numbersoffileperset)]
@@ -17,13 +21,28 @@ for idx,sets in enumerate(composite_list):
             image2 = cv2.imread(path+sets[j])
             image1 = imutils.resize(image1,width=600)
             image2 = imutils.resize(image2, width=600)
-            #stitched = im_stitcher(image1, image2)
-            #print("{0} was tested against {1}".format(sets[i], sets[j]))
-            #np.save('/mnt/hgfs/Kaggle Drapper/train/npy/1/BRISK_matching_{0}_vs_{1}.npy'.format(sets[i][:-4], sets[j][:-4]),stitched)
-            #print('BRISK_matching_{0}_vs_{1}.npy'.format(sets[i][:-4], sets[j][:-4]))
-            stitched = im_stitcher(image2,image1)
-            print("{0} was tested against {1}".format(sets[i],sets[j]))
-            #np.save('/mnt/hgfs/Kaggle Drapper/train/npy/0/BRISK_matching_set223_5_vs_set223_3.npy')
-            np.save('/mnt/hgfs/Kaggle Drapper/train/npy/0/BRISK_matching_{0}_vs_{1}.npy'.format(sets[j][:-4],sets[i][:-4]), stitched)
-            print('BRISK_matching_{0}_vs_{1}.npy'.format(sets[j][:-4],sets[i][:-4]))
+            #-----------------------First compared to second----------------------------#1
+            stitched = im_stitcher(image1, image2)
+            print("{0} was tested against {1}".format(sets[i], sets[j]))
+
+            #----------------------Second compared to first-----------------------------#2
+            #stitched = im_stitcher(image2, image1)
+            #print("{0} was tested against {1}".format(sets[j],sets[i]))
+            #print('BRISK_matching_{0}_vs_{1}.npy'.format(sets[j][:-4],sets[i][:-4]))
+            steps = 32
+            cols = len(stitched)
+            rows = len(stitched[0])
+            splitted = []
+            for x,sx in enumerate(range(0,len(stitched),steps)):
+                for y,sy in enumerate(range(0,len(stitched[0]),steps)):
+                    splitted = stitched[sx:sx+steps,sy:sy+steps,:]
+                    if np.any(splitted):
+                        #1
+                        print('/mnt/hgfs/Kaggle Drapper/Kaggle-Draper2/train/npy/1/BRISK_matching_{0}_vs_{1}_{2}_{3}.npy'.format(sets[i][:-4], sets[j][:-4],x,y))
+                        np.save('/mnt/hgfs/Kaggle Drapper/Kaggle-Draper2/train/npy/1/BRISK_matching_{0}_vs_{1}_{2}_{3}.npy'.format(sets[i][:-4], sets[j][:-4],x,y),splitted)
+                    else :
+                        print "Array is empty"
+                    #2
+                    #print('/mnt/hgfs/Kaggle Drapper/Kaggle-Draper2/train/npy/0/BRISK_matching_{0}_vs_{1}_{2}_{3}.npy'.format(sets[j][:-4], sets[i][:-4], x, y))
+                    #np.save('/mnt/hgfs/Kaggle Drapper/Kaggle-Draper2/train/npy/0/BRISK_matching_{0}_vs_{1}_{2}_{3}.npy'.format(sets[j][:-4],sets[i][:-4],x,y),splitted)
             j += 1
--- chipy.py
+++ chipy.py
@@ -6,7 +6,7 @@
 # Want to extract the warp parameters
 #
 ########################################################################
-
+from osgeo import ogr
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
@@ -16,12 +16,14 @@ import time
 
 beg = time.time()
 
-
+def flattenImage(im):
+    ret, thresh = cv2.threshold(im, 1, 255, cv2.THRESH_BINARY)
+    flat = cv2.cvtColor(thresh, cv2.COLOR_RGB2GRAY, 0)
+    return flat
 def im_stitcher(image1, image2, pcntDownsize=1.0, withTransparency=False):
     start = time.time()
     # Read image1
     #image1 = cv2.imread(imp1)
-
     # perform the resizing of the image by pcntDownsize and create a Grayscale version
     dim1 = (int(image1.shape[1] * pcntDownsize), int(image1.shape[0] * pcntDownsize))
     img1 = cv2.resize(image1, dim1, interpolation=cv2.INTER_AREA)
@@ -48,8 +50,6 @@ def im_stitcher(image1, image2, pcntDownsize=1.0, withTransparency=False):
     for m in matches:
         if len(m) == 2 and m[0].distance < m[1].distance * 0.75:
             matches_.append((m[0].trainIdx, m[0].queryIdx))
-
-    print len(matches_)
     kp1_ = np.float32([kp1[m[1]].pt for m in matches_]).reshape(-1, 1, 2)
     kp2_ = np.float32([kp2[m[0]].pt for m in matches_]).reshape(-1, 1, 2)
     H, mask = cv2.findHomography(kp2_, kp1_, cv2.RANSAC, 4.0)
@@ -58,108 +58,57 @@ def im_stitcher(image1, image2, pcntDownsize=1.0, withTransparency=False):
 
     pts1 = np.float32([[0, 0], [0, h1], [w1, h1], [w1, 0]]).reshape(-1, 1, 2)
     pts2 = np.float32([[0, 0], [0, h2], [w2, h2], [w2, 0]]).reshape(-1, 1, 2)
-
     pts2_ = cv2.perspectiveTransform(pts2, H)
+    #calculate the height and the width of the final image
     pts = np.concatenate((pts1, pts2_), axis=0)
-
     [xmin, ymin] = np.int32(pts.min(axis=0).ravel() - 0.5)
     [xmax, ymax] = np.int32(pts.max(axis=0).ravel() + 0.5)
-
     t = [-xmin, -ymin]
-
     Ht = np.array([[1, 0, t[0]], [0, 1, t[1]], [0, 0, 1]])
-
     # warp the colour version of image2
     im = cv2.warpPerspective(img2, Ht.dot(H), (xmax - xmin, ymax - ymin))
-
     # overlay colur version of image1 to warped image2
-    # if withTransparency == True:
     h3, w3 = im.shape[:2]
     bim = np.zeros((h3, w3, 3), np.uint8)
     bim[t[1]:h1 + t[1], t[0]:w1 + t[0]] = img1
+    #Creating the mask
+    #Getting the points in wkt format
+    ##ALMOST
+    wkt1 = "POLYGON (({0} {1}, {2} {3}, {4} {5}, {6} {7}, {0} {1}))".format(pts1[0][0][0]+t[0],pts1[0][0][1]+t[1],pts1[1][0][0]+t[0],pts1[1][0][1]+t[1],pts1[2][0][0]+t[0],pts1[2][0][1]+t[1],pts1[3][0][0]+t[0],pts1[3][0][1]+t[1])
+    wkt2 = "POLYGON (({0} {1}, {2} {3}, {4} {5}, {6} {7}, {0} {1}))".format(pts2_[0][0][0]+t[0],pts2_[0][0][1]+t[1],pts2_[1][0][0]+t[0],pts2_[1][0][1]+t[1],pts2_[2][0][0]+t[0],pts2_[2][0][1]+t[1],pts2_[3][0][0]+t[0],pts2_[3][0][1]+t[1])
+    #Creating the polygons
+    poly1 = ogr.CreateGeometryFromWkt(wkt1)
+    #cv2.imshow("poly1",poly1)
+    poly2 = ogr.CreateGeometryFromWkt(wkt2)
+    #Finding the intersected area
+    intersection = poly1.Intersection(poly2)
+    overlapWKTformat =  intersection.ExportToWkt()
+    #Parsing
+    overlapCoordinate = np.array(np.int32(np.float32([points.split(' ') for points in overlapWKTformat[10:len(overlapWKTformat)-2].split(',')])))
+    maskB = np.zeros((h3, w3, 3), np.uint8)
+    #maskB = np.zeros((img1.shape[0],img1.shape[1]))
+    cv2.fillConvexPoly(maskB,overlapCoordinate,(255,255,255))
+    xmin = min(x for x,y in overlapCoordinate)
+    xmax = max(x for x,y in overlapCoordinate)
+    ymin = min(y for x,y in overlapCoordinate)
+    ymax = max(y for x,y in overlapCoordinate)
+    im3 = cv2.addWeighted(im,0.6,bim,0.6,0)
+    im = cv2.bitwise_and(im,maskB)
+    bim = cv2.bitwise_and(bim,maskB)
+    im = im[ymin:ymax,xmin:xmax]
+    bim = bim[ymin:ymax,xmin:xmax]
+    #print len(bim[0])
+    #cv2.imshow("im_cropped",im)
+    #cv2.imshow("bim",bim)
+    #cv2.imshow("overlay",im3)
 
-    #   #imGray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    #   #imColor = cv2.applyColorMap(imGray, cv2.COLORMAP_JET)
-
-    #   #im =(im[:,:,2] - bim[:,:,2])
-    #   im = cv2.addWeighted(im,0.6,bim,0.6,0)
-    im = np.dstack((im, bim))
     # else:
     #   im[t[1]:h1+t[1],t[0]:w1+t[0]] = img1  # Here be the crux. Left side is "position of img1", right?
     print("Image took {0} s to complete.".format(round(time.time() - start, 1)))
     end = time.time()
+    im = np.dstack((im, bim))
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
     return (im)
-
-
-##########################################################
-#
-# Match all combinations of one set of images
-#
-##########################################################
-
-# img104_1_166_1 = im_stitcher("../input/test/set104_1.tif", "../input/test/set166_1.tif", 0.4, True)
-# img104_2_166_4 = im_stitcher("../input/test/set104_2.tif", "../input/test/set166_4.tif", 0.4, True)
-# img104_3_166_5 = im_stitcher("../input/test/set104_3.tif", "../input/test/set166_5.tif", 0.4, True)
-# img104_4_166_3 = im_stitcher("../input/test/set104_4.tif", "../input/test/set166_3.tif", 0.4, True)
-# img104_5_166_2 = im_stitcher("../input/test/set104_5.tif", "../input/test/set166_2.tif", 0.4, True)
-
-# plt.imsave('Set104_1_166_1_BRISK_matching.tif',img104_1_166_1)
-# plt.imsave('Set104_2_166_4_BRISK_matching.tif',img104_2_166_4)
-# plt.imsave('Set104_3_166_5_BRISK_matching.tif',img104_3_166_5)
-# plt.imsave('Set104_4_166_3_BRISK_matching.tif',img104_4_166_3)
-# plt.imsave('Set104_6_166_2_BRISK_matching.tif',img104_5_166_2)
-
-# img1_1_85_5 = im_stitcher("../input/test/set1_1.tif", "../input/test/set85_5.tif", 0.4, True)
-# img1_2_85_4 = im_stitcher("../input/test/set1_2.tif", "../input/test/set85_4.tif", 0.4, True)
-# img1_3_85_2 = im_stitcher("../input/test/set1_3.tif", "../input/test/set85_2.tif", 0.4, True)
-# img1_4_85_3 = im_stitcher("../input/test/set1_4.tif", "../input/test/set85_3.tif", 0.4, True)
-# img1_5_85_1 = im_stitcher("../input/test/set1_5.tif", "../input/test/set85_1.tif", 0.4, True)
-
-# plt.imsave('Set1_1_85_5_BRISK_matching.tif',img1_1_85_5)
-# plt.imsave('Set1_2_85_4_BRISK_matching.tif',img1_2_85_4)
-# plt.imsave('Set1_3_85_2_BRISK_matching.tif',img1_3_85_2)
-# plt.imsave('Set1_4_85_3_BRISK_matching.tif',img1_4_85_3)
-# plt.imsave('Set1_5_85_1_BRISK_matching.tif',img1_5_85_1)
-
-# img3_1_22_1 = im_stitcher("../input/test/set3_1.tif", "../input/test/set22_1.tif", 0.4, True)
-# img3_2_22_2 = im_stitcher("../input/test/set3_2.tif", "../input/test/set22_2.tif", 0.4, True)
-# img3_3_22_5 = im_stitcher("../input/test/set3_3.tif", "../input/test/set22_5.tif", 0.4, True)
-# img3_4_22_3 = im_stitcher("../input/test/set3_4.tif", "../input/test/set22_3.tif", 0.4, True)
-# img3_5_22_4 = im_stitcher("../input/test/set3_5.tif", "../input/test/set22_4.tif", 0.4, True)
-
-# plt.imsave('Set3_1_22_1_BRISK_matching.tif',img3_1_22_1)
-# plt.imsave('Set3_2_22_2_BRISK_matching.tif',img3_2_22_2)
-# plt.imsave('Set3_3_22_5_BRISK_matching.tif',img3_3_22_5)
-# plt.imsave('Set3_4_22_3_BRISK_matching.tif',img3_4_22_3)
-# plt.imsave('Set3_5_22_4_BRISK_matching.tif',img3_5_22_4)
-
-# img5_1_68_3 = im_stitcher("../input/train/set5_1.tif", "../input/test/set68_3.tif", 0.4, True)
-# plt.imsave('Set5_1_68_3_BRISK_matching.tif',img5_1_68_3)
-
-# img160_5_74_2 = im_stitcher("../input/train/set160_5.tif", "../input/test/set74_2.tif")
-# img160_5_74_3 = im_stitcher("../input/train/set160_5.tif", "../input/test/set74_3.tif")
-# img160_5_74_4 = im_stitcher("../input/train/set160_5.tif", "../input/test/set74_4.tif")
-# img160_5_74_5 = im_stitcher("../input/train/set160_5.tif", "../input/test/set74_5.tif")
-
-
-# This can save the file as a human-readable .txt or .csv, but it's 50x slower.
-# text = ''
-# for row in img160_5_74_1:
-#     for e in row:
-#         text += '{} {} {} {} {} {},'.format(e[0], e[1], e[2], e[3], e[4], e[5])
-#     text += '\n'
-#
-# # Write the string to a file.
-# with open('image.csv', 'w') as f:
-#     f.write(text)
-#
-
-# np.savetxt('Set160_5_74_2_BRISK_matching.csv',img160_5_74_2, delimiter=",", fmt="%s")
-# np.savetxt('Set160_5_74_3_BRISK_matching.csv',img160_5_74_3, delimiter=",", fmt="%s")
-# np.savetxt('Set160_5_74_4_BRISK_matching.csv',img160_5_74_4, delimiter=",", fmt="%s")
-# np.savetxt('Set160_5_74_5_BRISK_matching.csv',img160_5_74_5, delimiter=",", fmt="%s")
-
-
-
 
 
